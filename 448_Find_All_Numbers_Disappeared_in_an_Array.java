f88dead2c79b1c42af32547f6ee71ea665b4d808
0ANIKETSINGH0 <78471317+aniketsingh222@users.noreply.github.com>
2022-08-26 11:44:11 +0530

    Update 448_Find_All_Numbers_Disappeared_in_an_Array.java

--- 448_Find_All_Numbers_Disappeared_in_an_Array.java
+++ 448_Find_All_Numbers_Disappeared_in_an_Array.java
@@ -1,3 +1,26 @@
-public class 448_Find_All_Numbers_Disappeared_in_an_Array {
-    
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+     int i=0;
+        while(i<nums.length){
+            int correct=nums[i]-1;
+            if(nums[i]!=nums[correct]){
+                swap(nums,i,correct);
+            }
+            else{
+                i++;
+            }
+        }
+        List<Integer> ans=new ArrayList<>();
+        for(int index=0;index<nums.length;index++){
+            if(nums[index]!=index+1){
+               ans.add(index+1);
+            }
+        } 
+        return ans;
+    }
+    void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
 }
