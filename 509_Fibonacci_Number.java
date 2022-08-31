7b00ac38da02b28e1450d3568cad0abc837075b4
0ANIKETSINGH0 <78471317+aniketsingh222@users.noreply.github.com>
2022-08-31 21:51:56 +0530

    Update 509_Fibonacci_Number.java

--- 509_Fibonacci_Number.java
+++ 509_Fibonacci_Number.java
@@ -1,3 +1,13 @@
-public class 509_Fibonacci_Number {
-    
+class Solution {
+    public int fib(int n) {
+        if(n<=1){return n;}
+        int first = 0;
+        int second = 1;
+        for(int i = 2; i <= n; i++) {
+            int temp = first + second;
+            first = second;
+            second = temp;
+        }
+        return second;
+    }
 }
