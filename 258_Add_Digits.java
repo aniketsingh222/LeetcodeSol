4420e2b1a5666caec31bc690caa8b624b5668fc6
0ANIKETSINGH0 <78471317+aniketsingh222@users.noreply.github.com>
2022-09-03 13:45:29 +0530

    Update 258_Add_Digits.java

--- 258_Add_Digits.java
+++ 258_Add_Digits.java
@@ -1,3 +1,15 @@
-public class 258_Add_Digits {
-    
+class Solution {
+    public int addDigits(int num) {
+      int r,sum=0;
+        while(num>9){
+            while(num!=0){
+                r = num%10;
+                sum+=r;
+                num/=10;
+                }
+            num=sum;
+            sum=0;
+        }
+    return num;
+    }
 }
