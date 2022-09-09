08254de4722f5125a7f9b461d7310a55f0d75d88
0ANIKETSINGH0 <78471317+aniketsingh222@users.noreply.github.com>
2022-09-09 13:35:21 +0530

    Update 7_Reverse_Integer.java

--- 7_Reverse_Integer.java
+++ 7_Reverse_Integer.java
@@ -1,3 +1,15 @@
-public class 7_Reverse_Integer {
-    
+class Solution {
+    public int reverse(int x) {
+        int result=0;
+        while(x!=0){
+            int lastDigit=x%10;
+            int reverse=result*10+lastDigit;
+            if((reverse-lastDigit)/10!=result){
+                return 0;
+            }
+            x=x/10;
+            result=reverse;
+        }
+        return result;
+    }
 }
