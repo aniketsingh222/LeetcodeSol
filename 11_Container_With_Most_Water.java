105f9f5425dedca823d1b079194a00343fb2104c
0ANIKETSINGH0 <78471317+aniketsingh222@users.noreply.github.com>
2022-09-12 18:19:19 +0530

    Update 11_Container_With_Most_Water.java

--- 11_Container_With_Most_Water.java
+++ 11_Container_With_Most_Water.java
@@ -1,3 +1,13 @@
-public class 11_Container_With_Most_Water {
-    
+class Solution {
+    public int maxArea(int[] height) {
+        int maxWater = Integer.MIN_VALUE;
+        int i =0;
+        int j=height.length-1;
+        while(i<j){
+            maxWater = Math.max(maxWater, Math.min(height[i],height[j])*(j-i));
+            if(height[i]<height[j]){i++;}
+            else{j--;}
+        }
+        return maxWater;
+    }
 }
