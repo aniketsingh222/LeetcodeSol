7324f963435135978cf81d4dac4ba8d8b7aee5cc
0ANIKETSINGH0 <78471317+aniketsingh222@users.noreply.github.com>
2022-09-15 16:38:56 +0530

    Update 75_Sort_Colors.java

--- 75_Sort_Colors.java
+++ 75_Sort_Colors.java
@@ -1,3 +1,11 @@
-public class 75_Sort_Colors {
-    
-}
+class Solution {
+    public void sortColors(int[] nums) {
+           int[] counts = new int[3];
+           int n = nums.length;
+           for(int i=0; i<n; i++)counts[nums[i]]++;
+           int i=0;                     
+           for(; i < counts[0]; i++)nums[i] = 0;
+           for(; i < counts[0] + counts[1]; i++)nums[i] = 1;
+           for(; i < counts[0] + counts[1] + counts[2]; i++)nums[i] = 2;
+       }
+   }
