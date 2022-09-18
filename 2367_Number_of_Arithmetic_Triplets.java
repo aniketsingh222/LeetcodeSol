443a2f60ca3da9a2421e3f9c522d33b89ed2bea2
Aniket Singh <aniketsingh22251@gmail.com>
2022-09-18 12:25:17 +0530

    Update 2367_Number_of_Arithmetic_Triplets.java

--- 2367_Number_of_Arithmetic_Triplets.java
+++ 2367_Number_of_Arithmetic_Triplets.java
@@ -1,3 +1,13 @@
-public class 2367_Number_of_Arithmetic_Triplets {
-    
+class Solution {
+    public int arithmeticTriplets(int[] nums, int diff) {
+        int[] cache = new int[201];
+        int count = 0;
+        for (int n : nums){
+            int prev = n - diff;
+            if (prev >= 0){cache[n] = cache[prev] + 1;} 
+            else {cache[n] = 1;}
+            if (cache[n] >= 3){count++;}
+        }
+        return count;
+    }
 }
