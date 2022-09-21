e3331e776fcb0f6749882f676258a8dd23270d15
Aniket Singh <aniketsingh22251@gmail.com>
2022-09-21 23:06:13 +0530

    Update 172_Factorial_Trailing_Zeroes.java

--- 172_Factorial_Trailing_Zeroes.java
+++ 172_Factorial_Trailing_Zeroes.java
@@ -1,3 +1,11 @@
-public class 172_Factorial_Trailing_Zeroes {
-    
+public class Solution {
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while (n != 0) {
+            int tmp = n / 5;
+            count += tmp;
+            n = tmp;
+        }
+        return count;
+    }
 }
