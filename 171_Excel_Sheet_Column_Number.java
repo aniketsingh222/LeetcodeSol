cbc3ae213276027fa6f4d1cb6214ddb0da6a8fdd
Aniket Singh <aniketsingh22251@gmail.com>
2022-09-22 22:15:13 +0530

    Update 171_Excel_Sheet_Column_Number.java

--- 171_Excel_Sheet_Column_Number.java
+++ 171_Excel_Sheet_Column_Number.java
@@ -1,3 +1,13 @@
-public class 171_Excel_Sheet_Column_Number {
-    
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int[] arr = new int[26];
+        int pow = 0;
+        int n = columnTitle.length();
+        int ans = 0;
+        for(int i = n-1; i>=0; i--){
+            int num = (columnTitle.charAt(i) - 'A') + 1;
+            ans += (int)Math.pow(26,pow++)*num;
+        }
+        return ans;
+    }
 }
