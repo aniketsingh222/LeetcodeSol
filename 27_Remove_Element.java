cb24234793b750d03c174138d498d2745ef0288e
Aniket Singh <aniketsingh22251@gmail.com>
2022-09-28 22:59:48 +0530

    Update 27_Remove_Element.java

--- 27_Remove_Element.java
+++ 27_Remove_Element.java
@@ -1,3 +1,11 @@
-public class 27_Remove_Element {
-    
+class Solution {
+    public int removeElement(int[] A, int elem) {
+    int l = A.length;
+    for (int i=0; i<l; i++) {
+        if (A[i] == elem) {
+            A[i--] = A[l-- -1];
+        }
+    }
+    return l;
+}
 }
