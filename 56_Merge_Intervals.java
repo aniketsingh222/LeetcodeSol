ef09c94f89d658b6224fc9a295c081eecd1ddd8c
Aniket Singh <aniketsingh22251@gmail.com>
2022-09-30 10:50:47 +0530

    Update 56_Merge_Intervals.java

--- 56_Merge_Intervals.java
+++ 56_Merge_Intervals.java
@@ -1,3 +1,28 @@
-public class 56_Merge_Intervals {
-    
+class Solution {
+    public int[][] merge(int[][] intervals) {
+    Arrays.sort(intervals,(o1,o2)->Integer.compare(o1[0], o2[0]));
+    List<int[]> ansList = new ArrayList<int[]>();
+    int start = intervals[0][0];
+    int end = intervals[0][1];        
+    int e,s;
+    int i =0;
+    while(i<intervals.length)
+    {
+        s = intervals[i][0];
+        e = intervals[i][1];
+        if(s<=end)
+        {
+            end = Math.max(end,e);
+        }
+        else
+        {
+            ansList.add(new int[]{start,end});
+            start = s;
+            end =  e;  
+        }
+        i++;
+    }
+    ansList.add(new int[] {start,end});
+    return ansList.toArray(new int[ansList.size()][]);
+    }
 }
